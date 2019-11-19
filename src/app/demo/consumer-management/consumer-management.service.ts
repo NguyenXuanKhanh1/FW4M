8056f3a68ff9f25c7e1e56a6e02838b3987049c9
Viet Anh Nguyen <nva1308@gmail.com>
2019-11-19 16:33:12 +0700

    valid fail

--- src/app/demo/consumer-management/consumer-management.service.ts
+++ src/app/demo/consumer-management/consumer-management.service.ts
@@ -6,30 +6,38 @@ import { ConsumerResponse, ConsumerRequest } from './consumer.model';
 import { Consumer } from './consumer';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ConsumerManagementService {
 
-  public apiUrl = 'http://192.168.35.108:8001/consumers'
-  public header = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+	public apiUrl = 'http://192.168.35.108:8001/consumers'
+	public header = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  public readData(request: ConsumerRequest): Observable<ConsumerResponse> {
-    return this.http.get(this.apiUrl).pipe(map((res: any) => {
-      for (let i = 0; i < res.data.length; i++) {
-        res.data[i].created_at = res.data[i].created_at * 1000;        
-      }
-      var response = ({
-        status: true,
-        totalRecords: res.data.length,
-        items: res.data
-      });
-      return response;
-    }));
-  }
+	public readData(request: ConsumerRequest): Observable<ConsumerResponse> {
+		return this.http.get(this.apiUrl).pipe(map((res: any) => {
+			for (let i = 0; i < res.data.length; i++) {
+				res.data[i].created_at = res.data[i].created_at * 1000;
+			}
+			var response = ({
+				status: true,
+				totalRecords: res.data.length,
+				items: res.data
+			});
+			return response;
+		}));
+	}
 
-  public deleteData(id: string) {
-    return this.http.delete(this.apiUrl + '/' + id);
-  }
+	public deleteData(id: string) {
+		return this.http.delete(this.apiUrl + '/' + id);
+	}
+
+	public validateString(string: string): Observable<ValidationRuleResponse> {
+		let regex = /[A-Za-z0-9]/;
+		return of(new ValidationRuleResponse({
+			status: regex.test(string) && string !== "",
+			message: 'Please input right format'
+		}));
+	}
 }
